df3d6ab032fac1cdb8ba1332a76ca17dff43af66
sebastianqp <sdquinga2@espe.edu.ec>
2025-11-14 00:13:09 -0500

    final CI

--- src/test/java/edu/espe/springlab/SpringLabApplicationTests.java
+++ src/test/java/edu/espe/springlab/SpringLabApplicationTests.java
@@ -2,8 +2,11 @@ package edu.espe.springlab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class SpringLabApplicationTests {
 
 	@Test
